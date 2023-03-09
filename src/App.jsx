c246980e3d40f0378080fc59b6dd05b0cdaddfc9
mhenriette <mhenriette00@gnmail.com>
2023-03-09 11:02:35 +0200

    chore: used arrow function in app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import './App.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-    </div>
-
-  )
+const App = () => {
+  return <div className="App">
+    <Navbar />
+    <Hero />
+  </div>
 }
 
 export default App
